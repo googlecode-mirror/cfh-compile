22f30fe0cdc9692621a4c3a5749ab242604ec28f
william.bailey <william.bailey@cowboysfromhell.co.uk>
2007-12-18 23:43:16 +0000

    Add cfhCompile_ClassIterator_Interface

---
+++ library/cfhCompile/ClassIterator/Interface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  ClassIterator
+ * @copyright   Copyright (c) 2007 William Bailey
+ * @license     http://www.gnu.org/licenses/lgpl.html     Lesser GPL
+ * @version     $Id$
+ */
+
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  ClassIterator
+ * @copyright   Copyright (c) 2007 William Bailey
+ */
+interface cfhCompile_ClassIterator_Interface
+extends Iterator
+{
+}
