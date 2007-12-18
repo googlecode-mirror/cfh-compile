912142da8b826bc90aefbda3614ae254bafa0679
william.bailey@cowboysfromhell.co.uk <william.bailey@cowboysfromhell.co.uk@43db6858-9541-0410-8bbd-1f3998f70fbc>
2007-12-18 23:40:55 +0000

    Add class registry.

---
+++ trunk/library/cfhCompile/ClassRegistry.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @copyright   Copyright (c) 2007 William Bailey
+ * @license     http://www.gnu.org/licenses/lgpl.html     Lesser GPL
+ * @version     $Id$
+ */
+
+/**
+ * Global class registry.
+ *
+ * @category    cfh
+ * @package     cfhCompile
+ * @copyright   Copyright (c) 2007 William Bailey
+ */
+class cfhCompile_ClassRegistry
+implements IteratorAggregate
+{
+
+    /**
+     * @var cfhCompile_ClassRegistry
+     */
+    static private $instance;
+
+    /**
+     * @var ArrayObject
+     */
+    protected $registry;
+
+    /**
+     * Gets the registry instance
+     * @return cfhCompile_ClassRegistry
+     */
+    final static public function getInstance()
+    {
+        if(!self::$instance)
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    final protected function __construct()
+    {
+        $this->registry = new ArrayObject(array());
+    }
+
+    final protected function __clone()
+    {
+    }
+
+    /**
+     * Register a class in the registry
+     *
+     * @param cfhCompile_Class_Interface $class
+     * @return cfhCompile_Class_Interface
+     * @throws cfhCompile_ClassRegistry_Exception
+     */
+    public function register(cfhCompile_Class_Interface $class)
+    {
+        if(isset($this->registry[$class->getName()]))
+        {
+            if($this->registry[$class->getName()] === $class)
+            {
+                return $class;
+            }
+            throw new cfhCompile_ClassRegistry_Exception(
+                      'Class '.$class->getName().' has already been registered.'
+                      );
+        }
+        $this->registry[$class->getName()] = $class;
+        return $class;
+    }
+
+    /**
+     * Fetches a class from the registry.
+     * @return cfhCompile_Class_Interface
+     */
+    public function fetch($className)
+    {
+        if(!isset($this->registry[$className]))
+        {
+            return NULL;
+        }
+        return $this->registry[$className];
+    }
+
+    /**
+     * Clears out the registry
+     */
+    public function clear()
+    {
+        $this->registry->exchangeArray(array());
+    }
+
+    /**
+     * Returns the current items in the registry.
+     * @return ArrayObject
+     */
+    public function getIterator()
+    {
+        return $this->registry;
+    }
+
+}
