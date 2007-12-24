7335f3afd5b3eaf9cb0af3167586e627883afa45
william.bailey <william.bailey@cowboysfromhell.co.uk>
2007-12-24 11:50:29 +0000

    Add cfhCompile_ClassIterator_FileInfo -- A bit messy at the moment and need to add a few more unit tests.

---
+++ library/cfhCompile/ClassIterator/FileInfo.php
@@ -0,0 +1,239 @@
+<?php
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  ClassIterator
+ * @copyright   Copyright (c) 2007 William Bailey
+ * @license     http://www.gnu.org/licenses/lgpl.html     Lesser GPL
+ * @version     $Id$
+ */
+
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  ClassIterator
+ * @copyright   Copyright (c) 2007 William Bailey
+ */
+class cfhCompile_ClassIterator_FileInfo
+implements cfhCompile_ClassIterator_Interface
+{
+
+    /**
+     * @var cfhCompile_Class_Interface
+     */
+    protected $current;
+
+    /**
+     * @var AppendIterator
+     */
+    protected $fileIterator;
+
+    /**
+     * @var SplObjectStorage
+     */
+    protected $nonTraversableItems;
+
+    /**
+     * @var array
+     */
+    protected $inspectedFiles = array();
+
+    protected $currentFileClasses = array();
+
+    public function __construct()
+    {
+        $this->nonTraversableItems = new SplObjectStorage();
+        $this->fileIterator        = new AppendIterator();
+        $this->fileIterator->append($this->nonTraversableItems);
+        $this->rewind();
+    }
+
+    public function current()
+    {
+        return $this->current;
+    }
+
+    public function key()
+    {
+        $c = $this->current();
+        if($c instanceof cfhCompile_Class_Interface)
+        {
+            return $c->getName();
+        }
+        return NULL;
+    }
+
+    public function next()
+    {
+        $this->current = NULL;
+        if(count($this->currentFileClasses) > 0)
+        {
+            $this->current = array_shift($this->currentFileClasses);
+            return;
+        }
+        do
+        {
+            $file = $this->fileIterator->current();
+            if($this->fileIterator->valid())
+            {
+                $this->fileIterator->next();
+            }
+        }
+        while($file
+             && (
+                !$file->isFile()
+                || isset($this->inspectedFiles[$file->getRealPath()])
+                )
+             );
+        if($file === NULL)
+        {
+            return;
+        }
+        $this->inspectedFiles[$file->getRealPath()] = TRUE;
+        // We add a new line to the code so that the tokeniser will give us a
+        // last token item for classes that end on the last line of a file
+        // without any trailing white space.
+        $tokens      = token_get_all(@file_get_contents($file->getRealPath()).PHP_EOL);
+        $depth       = -1;
+        $className   = NULL;
+        $startLine   = NULL;
+        $endLine     = NULL;
+        $isInterface = FALSE;
+        $dependancys = array();
+        $getDep      = FALSE;
+        reset($tokens);
+        do
+        {
+            $token = current($tokens);
+            switch($depth)
+            {
+                case -1:
+                    if(!is_string($token))
+                    {
+                        list($token, $text, $line) = $token;
+                        switch($token)
+                        {
+                            case T_INTERFACE:
+                                $isInterface = TRUE;
+                            case T_CLASS:
+                                $depth     = 0;
+                                $startLine = $line;
+                                $endLine   = NULL;
+                                break;
+                        }
+                    }
+                    break;
+                case 0:
+                    list($token, $text) = $token;
+                    switch($token)
+                    {
+                        case T_STRING:
+                            $className = $text;
+                            $depth = 1;
+                            break;
+                    }
+                    break;
+                case 1:
+                    if(is_string($token))
+                    {
+                        if($token == '{')
+                        {
+                            $depth = 3;
+                        }
+                    }
+                    else
+                    {
+                        list($token, $text) = $token;
+                        switch($token)
+                        {
+                            case T_EXTENDS:
+                                $getDep = TRUE;
+                                break;
+                            case T_IMPLEMENTS:
+                                $getDep = TRUE;
+                                break;
+                            case T_STRING:
+                                if($getDep)
+                                {
+                                    $dependancys[] = $text;
+                                }
+                                break;
+                            case T_WHITESPACE:
+                                break;
+                            default:
+                                $getDep = FALSE;
+                                break;
+                        }
+                    }
+                    break;
+                default:
+                    if(is_string($token))
+                    {
+                        switch ($token)
+                        {
+                            case '{';
+                                $depth++;
+                                break;
+                            case '}';
+                                $depth--;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        list($token, $text, $endLine) = $token;
+                        if($depth == 2)
+                        {
+                            $depth = -1;
+                            prev($tokens);
+                            $class = new cfhCompile_Class_Manual(
+                                         $className,
+                                         $file->getRealPath(),
+                                         $startLine,
+                                         $endLine,
+                                         $isInterface,
+                                         new ArrayIterator($dependancys)
+                                         );
+                            array_push(
+                                      $this->currentFileClasses,
+                                      $class
+                                      );
+                            $className   = NULL;
+                            $startLine   = NULL;
+                            $endLine     = NULL;
+                            $isInterface = FALSE;
+                            $dependancys = array();
+                            $getDep      = FALSE;
+                        }
+                    }
+                    break;
+            }
+        }while(next($tokens));
+        $this->next();
+    }
+
+    public function rewind()
+    {
+        $this->fileIterator->rewind();
+        $this->inspectedFiles = array();
+        $this->next();
+    }
+
+    public function valid()
+    {
+        return $this->current() instanceof cfhCompile_Class_Interface;
+    }
+
+    public function attach(splFileInfo $fileInfo)
+    {
+        if($fileInfo instanceof Iterator)
+        {
+            $this->fileIterator->append($fileInfo);
+        }
+        else
+        {
+            $this->nonTraversableItems->attach($fileInfo);
+        }
+    }
+
+}
---
+++ tests/cfhCompile/ClassIterator/FileInfoTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  UnitTests
+ * @copyright   Copyright (c) 2007 William Bailey
+ * @license     http://www.gnu.org/licenses/lgpl.html     Lesser GPL
+ * @version     $Id$
+ */
+
+/**
+ * @group       cfhCompile
+ * @group       cfhCompile_ClassIterator
+ * @group       cfhCompile_ClassIterator_FileInfo
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  UnitTests
+ * @copyright   Copyright (c) 2007 William Bailey
+ */
+class cfhCompile_ClassIterator_FileInfoTest
+extends PHPUnit_Framework_TestCase
+{
+
+    public function testClases001()
+    {
+        $file = realpath(dirname(__FILE__).'/.resource/classes.001.php');
+        $i = new cfhCompile_ClassIterator_FileInfo();
+        $i->attach(new SplFileInfo($file)); // duplicate files should only be
+        $i->attach(new SplFileInfo($file)); // processed once.
+        $i->rewind();
+
+        $this->assertTrue($i->valid());
+        $class = $i->current();
+        $this->assertEquals($i->key(), $class->getName());
+        $this->assertEquals('foo', $class->getName());
+        $this->assertEquals($file, $class->getFileName());
+        $this->assertEquals(2, $class->getStartLine());
+        $this->assertEquals(4, $class->getEndLine());
+        $this->assertTrue($class->isInterface());
+        $this->assertEquals(0, $class->getDependancys()->count());
+
+        $i->next();
+        $this->assertTrue($i->valid());
+        $class = $i->current();
+        $this->assertEquals($i->key(), $class->getName());
+        $this->assertEquals('bar', $class->getName());
+        $this->assertEquals($file, $class->getFileName());
+        $this->assertEquals(6, $class->getStartLine());
+        $this->assertEquals(6, $class->getEndLine());
+        $this->assertFalse($class->isInterface());
+        $this->assertEquals(0, $class->getDependancys()->count());
+
+        $i->next();
+        $this->assertTrue($i->valid());
+        $class = $i->current();
+        $this->assertEquals($i->key(), $class->getName());
+        $this->assertEquals('foobar', $class->getName());
+        $this->assertEquals($file, $class->getFileName());
+        $this->assertEquals(6, $class->getStartLine());
+        $this->assertEquals(6, $class->getEndLine());
+        $this->assertFalse($class->isInterface());
+        $this->assertEquals(2, $class->getDependancys()->count());
+        foreach($class->getDependancys() as $offset => $className)
+        {
+            if($offset == 0)
+            {
+                $this->assertEquals('bar', $className);
+            }
+            else
+            {
+                $this->assertEquals('foo', $className);
+            }
+        }
+
+        $i->next();
+        $this->assertFalse($i->valid());
+        $this->assertNull($i->key());
+        $this->assertNull($i->current());
+    }
+
+    public function testRecurseDirectory()
+    {
+        $this->markTestIncomplete('Not implemented.');
+    }
+
+}
