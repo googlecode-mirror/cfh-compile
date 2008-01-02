0c31d0808a9b1b18fe6b1ffc3935c156a95aff84
william.bailey <william.bailey@cowboysfromhell.co.uk>
2008-01-02 19:32:27 +0000

    Add cfhCompile_Runtime_ClassLogger class.

---
+++ library/cfhCompile/Runtime/ClassLogger/Strategy/Null.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  Runtime
+ * @copyright   Copyright (c) 2007 William Bailey
+ * @license     http://www.gnu.org/licenses/lgpl.html     Lesser GPL
+ * @version     $Id$
+ */
+
+/**
+ * @category    cfh
+ * @package     cfhCompile
+ * @subpackage  Runtime
+ * @copyright   Copyright (c) 2007 William Bailey
+ */
+class cfhCompile_Runtime_ClassLogger_Strategy_Null
+implements cfhCompile_Runtime_ClassLogger_Strategy_Interface
+{
+
+    public function begin()
+    {
+    }
+
+    /**
+     * @param cfhCompile_Class_Interface $class
+     */
+    public function log(cfhCompile_Class_Interface $class)
+    {
+    }
+
+    public function commit()
+    {
+    }
+
+    public function rollback()
+    {
+    }
+
+}
